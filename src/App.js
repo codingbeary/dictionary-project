0503df045cfe213e2f8ca960eb6caf8eeecbdd90
kiyooteru <kiyooteru@gmail.com>
2023-01-25 14:01:10 +0100

    built basic search engine

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="app-footer">
+          <small>This project was coded by Kinga Miśkiewicz</small>
+        </footer>
+      </div>
+    </div>
+  );
 }
